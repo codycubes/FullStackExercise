8aae76859dd18240462a2742c341b663a31e9a61
codycubes <boitshepomashamaite777@.com>
2024-05-13 10:40:59 +0200

    first commit

---
+++ backend/config.js
@@ -0,0 +1,8 @@
+export const PORT = 5555;
+
+
+
+export const mongoDBURL = 'mongodb+srv://ROOT:1234@bookstoremern.w9tlkdt.mongodb.net/books-collection?retryWrites=true&w=majority&appName=bookstoreMERN'
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
